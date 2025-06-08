978a2dce0fa3bec13c3ffbbe282397c21f40f1b8
Dihara Sathmi <diharasathmi2004@gmail.com>
2025-06-08 22:45:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// External JavaScript file
+
+var userEmail = 'diharasathmi2004@gmail.com';
+const userName = 'Dihara Sathmi';
+let userAge = 21;
+
+// This is a single line comment
+
+/*
+This is a
+multi-line
+comment
+*/
+
+userAge = 29;
+
+console.log("I'm second javascript file here!", {userEmail, userName, userAge});
